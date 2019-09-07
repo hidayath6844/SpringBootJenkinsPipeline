1a5ad0fc123cbeacc4d52f7f28070f82bc5bf4ca
hidayath6844 <hidayath6844@yahoo.com>
2019-09-07 10:03:37 +0530

    Spring boot pipe line jenkins demo project

---
+++ src/main/java/com/sb/jenkins/pipeline/SpringBootJenkinsPipelineApplication.java
@@ -0,0 +1,21 @@
+package com.sb.jenkins.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsPipelineApplication.class, args);
+	}
+
+	@GetMapping("/JenkinsPipelineTest")
+	public String jenkinsTest()
+	{
+		return "Welcome to Jenkins Pipeline CI/CD.";
+	}
+}
