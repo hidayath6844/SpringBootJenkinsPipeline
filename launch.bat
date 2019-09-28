692151c517849960d2e7d0ae5c108f100eeee48e
hidayath6844 <hidayath6844@yahoo.com>
2019-09-28 19:16:44 +0530

    ch

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 copy .\build\libs\SpringBootJenkinsPipeline-0.0.1-SNAPSHOT.jar D:\JenkinsDeployment
+cd D:\JenkinsDeployment
 java -jar D:\JenkinsDeployment\SpringBootJenkinsPipeline-0.0.1-SNAPSHOT.jar
 
-exit
 
